d5e944c69120d3124fba4e041055506a8defd547
Jordan Muir <jordanhmuir@gmail.com>
2024-03-20 11:04:24 -0700

    Organized files and added primitive player death

---
+++ SideScroller/Game/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+class_name MainMenu
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func _on_start_button_down():
+	get_tree().change_scene_to_file("res://Levels/randroomgenerating.tscn")
+
+func _on_quit_button_down():
+	get_tree().quit()
+
+
+func _on_coop_button_down():
+	pass # Replace with function body.
+
+
+func _on_settings_button_down():
+	pass # Replace with function body.
---
+++ SideScroller/Player/player.gd
@@ -0,0 +1,84 @@
+extends CharacterBody2D
+
+class_name Player
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var anim_tree: AnimationTree = %AnimationTree
+
+var stats = {
+	max_health = 100,
+	speed = 150.0,
+	jump_power = -400.0,
+	jumps = 1
+}
+
+var item_counts = {
+	"ACM_BRAND_PIZZA" : 0,
+	"feather": 0
+}
+var health = stats.max_health
+
+func _ready():
+	anim_tree.active = true
+
+func update_animation_parameters():
+	if velocity.x < -0.1:
+		%PlayerSprite.flip_h = true
+	elif velocity.x > 0.1:
+		%PlayerSprite.flip_h = false
+	
+	if velocity == Vector2.ZERO:
+		anim_tree["parameters/conditions/idle"] = true
+		anim_tree["parameters/conditions/is_moving"] = false
+	else:
+		anim_tree["parameters/conditions/is_moving"] = true
+		anim_tree["parameters/conditions/idle"] = false
+	
+	if(Input.is_action_just_pressed("attack")):
+		anim_tree["parameters/conditions/swing"] = true
+	else:
+		anim_tree["parameters/conditions/swing"] = false
+	
+func equip_item(item: Item):
+	item_counts[item.name] += 1
+	item.equip(self)
+	print(stats)
+	print(item_counts)
+
+func get_stats():
+	return stats
+
+func take_damage(val: int):
+	health -= val
+	print(health)
+	# Handling death
+	if health <= 0:
+		player_death()
+
+func player_death():
+	#On death, right now just resets the current scene
+	print("Dead!")
+	get_tree().reload_current_scene()
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = stats.jump_power
+	
+	update_animation_parameters()
+
+
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * stats.speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, stats.speed)
+		
+	move_and_slide()
