ba4559ca6b1929f3dbf27c0832d0e88cf586119f
Emily Krohn <emilykrohn2@gmail.com>
2024-03-23 10:49:44 -0700

    Flying enemy moves using NavigationAgent2D

--- SideScroller/Enemies/Flying/FlyingEnemy.gd
+++ SideScroller/Enemies/Flying/FlyingEnemy.gd
@@ -4,30 +4,45 @@ class_name FlyingEnemy
 
 @export var enemy_stats: FlyingEnemyStats
 @export var player: Player
+@onready var nav_agent = $NavigationAgent2D
+
 var direction = Vector2.RIGHT
 var damaging = false
+var in_range = false
 
 func _ready():
+	nav_agent.path_desired_distance = 4
+	nav_agent.target_desired_distance = 4
 	if !player:
-		player = get_parent().get_node("CharacterBody2D")
+		player = get_parent().get_node("Player")
 
 func _physics_process(delta):
-
-	if player:
-		direction = (player.global_position - global_position).normalized()
-		global_position += enemy_stats.SPEED * direction * delta
+	if in_range:
+		nav_agent.target_position = player.global_position
+	
+	if nav_agent.is_navigation_finished():
+		return
+	
+	var direction = (nav_agent.get_next_path_position() - global_position).normalized()
+	velocity = direction * enemy_stats.SPEED
 
 	if damaging:
 		player.take_damage(enemy_stats.DAMAGE)
 
 	move_and_slide()
 
-
 func _on_area_2d_body_entered(body):
 	if body.has_method("take_damage"):
 		damaging = true
 
-
 func _on_area_2d_body_exited(body):
 	if body.has_method("take_damage"):
 		damaging = false
+
+func _on_range_body_entered(body):
+	if body.name == "Player":
+		in_range = true
+
+func _on_range_body_exited(body):
+	if body.name == "Player":
+		in_range = false
