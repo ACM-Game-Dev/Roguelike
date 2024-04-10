76d808fbc337b1bdc056bc105370dba7ceda8737
Jordan Muir <jordanhmuir@gmail.com>
2024-04-10 13:25:27 -0700

    Hammer and Sword classes finished with working hitboxes
    Just need to actually damage the  enemies now :)

--- SideScroller/Equipment/Hammer/hammer.gd
+++ SideScroller/Equipment/Hammer/hammer.gd
@@ -3,6 +3,7 @@ extends Equipment
 class_name Hammer
 
 @export var weapon_resource: Weapon_Resource
+@onready var hitbox = $Area2D/CollisionShape2D
 var cooledDown = true
 
 func equip(player):
@@ -12,21 +13,38 @@ func equip(player):
 	player.curr_weapon = weapon_resource.equipment_name
 	player.idle_anim = weapon_resource.idle_anim
 	player.attack_anim = weapon_resource.activate_anim
+	hitbox.disabled = true
 	
 func activate(player):
 	if not cooledDown:
 		return
 		
+	rotate_hitbox(player)
+		
 	cooledDown = false
 	player.attack_animation() # Playing the player's attack animation
+	hitbox.disabled = false
 	print("HAMMER SWUNG")
+	await get_tree().create_timer(1).timeout #Keeps the hitbox active for the duration of the animation
+	hitbox.disabled = true
+	await get_tree().create_timer(weapon_resource.attack_delay - 1).timeout #We subtract the amount of time the hitbox was active for
 	
-	await get_tree().create_timer(weapon_resource.attack_delay).timeout
 	cooledDown = true
 	print("READY TO SWING HAMMER")
 	
 	
-
+func rotate_hitbox(player):
+	#Checks if the player is facing the opposite direciton
+	if player.sprite.flip_h:
+		self.scale.x = -1
+	else:
+		self.scale.x = 1
+		
 func drop(player):
 	# Reset player animations to normal
 	pass
+
+
+func hitbox1_detection(body):
+	#Do damage to thing
+	pass # Replace with function body.
--- SideScroller/Equipment/Sword/sword.gd
+++ SideScroller/Equipment/Sword/sword.gd
@@ -3,6 +3,8 @@ extends Equipment
 class_name Sword
 
 @export var weapon_resource: Weapon_Resource
+@onready var hitbox1 = $AttackArea1/CollisionShape2D
+@onready var hitbox2 = $AttackArea2/CollisionShape2D
 var swings = 0
 var cooledDown = true
 
@@ -13,17 +15,26 @@ func equip(player):
 	player.curr_weapon = weapon_resource.equipment_name
 	player.idle_anim = weapon_resource.idle_anim
 	player.attack_anim = weapon_resource.activate_anim
+	hitbox1.disabled = true
+	hitbox2.disabled = true
 	
 func activate(player):
 	if not cooledDown:
 		return
 		
+	rotate_hitbox(player)
+	
 	swings += 1
 	# We change the player's attack animation depending on the current state of our sword combo
-	if swings == 2:
+	if swings == 1:
+		player.attack_anim = weapon_resource.activate_anim
+		hitbox1.disabled = false
+	elif swings == 2:
 		player.attack_anim = "Sword_Swing2" 
+		hitbox2.disabled = false
 	elif swings > 2:
 		player.attack_anim = "Sword_Swing3"
+		hitbox1.disabled = false #The motion for 3 is similar to 1, hence reusing the same hitbox
 		
 	cooledDown = false
 	player.attack_animation() # Playing the player's attack animation
@@ -32,13 +43,30 @@ func activate(player):
 	# Resetting if we exceed our combo (3 swings) and starting the attack delay
 	if swings >= 3:
 		swings = 0
-		player.attack_anim = weapon_resource.activate_anim
 	await get_tree().create_timer(weapon_resource.attack_delay).timeout
+	hitbox1.disabled = true
+	hitbox2.disabled = true
 	cooledDown = true
 	print("READY TO SWING SWORD")
 	
 	
+func rotate_hitbox(player):
+	#Checks if the player is facing the opposite direciton
+	if player.sprite.flip_h:
+		self.scale.x = -1
+	else:
+		self.scale.x = 1
 
 func drop(player):
 	# Reset player animations to normal
 	pass
+
+
+func hitbox1_detection(body):
+	#Do damage to thing
+	pass # Replace with function body.
+
+
+func hitbox2_detection(body):
+	#Do damage to thing
+	pass # Replace with function body.
