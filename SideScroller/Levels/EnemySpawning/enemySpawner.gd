0a15af60f0affce65fa9de0643baedc498817712
Gabrielius Gintalas <gabrieliusgintalas@csu.fullerton.edu>
2024-04-12 16:00:23 -0700

    organized levels + working on enemy spawning

--- SideScroller/Levels/EnemySpawning/enemySpawner.gd
+++ SideScroller/Levels/EnemySpawning/enemySpawner.gd
@@ -1,11 +1,52 @@
 extends Node
 
+@export var spawn_points: Array[NodePath] = []  
+var player: Node = null
+var enemy_spawn_rate_node: Node = null 
+var has_player_triggered_spawn = false
 
-# Called when the node enters the scene tree for the first time.
+var flying_enemy_scene = load("res://Enemies/Flying/flying_enemy.tscn")
+var ground_enemy_scene = load("res://Enemies/Ground/ground_enemy.tscn")
+
 func _ready():
-	pass # Replace with function body.
+	player = get_node_or_null("/root/MainStage/Player")
+	enemy_spawn_rate_node = get_node_or_null("/root/MainStage")  
 
+func trigger_spawn(body):
+	if body == player and not has_player_triggered_spawn:
+		spawn_enemies()
+		has_player_triggered_spawn = true
+		print("Should spawn enemies!!")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func spawn_enemies():
+	var rate = enemy_spawn_rate_node.get_spawn_rate()
+	var spawn_count = int(rate * spawn_points.size())
+	var chosen_points = []
+	for path in spawn_points:
+		chosen_points.append(get_node(path)) 
+
+	chosen_points.shuffle() 
+	for i in range(spawn_count):
+		var spawn_position = chosen_points[i].global_position
+		spawn_enemy_at(spawn_position)
+
+func spawn_enemy_at(position):
+	var flying_rate = enemy_spawn_rate_node.get_flying_enemy_spawn_rate()
+	var ground_rate = enemy_spawn_rate_node.get_ground_enemy_spawn_rate()
+	var total_rate = flying_rate + ground_rate
+
+	if total_rate == 0:
+		return 
+
+	var spawn_threshold = flying_rate / total_rate
+	var random_choice = randf()
+
+	var enemy_instance
+	if random_choice < spawn_threshold:
+		enemy_instance = flying_enemy_scene.instantiate()
+	else:
+		enemy_instance = ground_enemy_scene.instantiate() 
+
+	enemy_instance.global_position = position
+	add_child(enemy_instance)
+	
---
+++ SideScroller/Levels/enemySpawnRate.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+@export var spawn_rate = 0
+@export var flying_spawn_rate = 0
+@export var ground_spawn_rate = 0
+
+func get_spawn_rate() -> float:
+	return spawn_rate
+	
+func get_flying_enemy_spawn_rate() -> float:
+	return flying_spawn_rate
+	
+func get_ground_enemy_spawn_rate() -> float:
+	return ground_spawn_rate
